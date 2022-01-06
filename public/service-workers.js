a97d0ee6d8e4c1d72144408d420c144822fe3593
Greg Riveroll <griveroll86@gmail.com>
2022-01-05 21:27:06 -0500

    indexdb

--- public/service-workers.js
+++ public/service-workers.js
@@ -5,25 +5,30 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/style.css",
     "/index.js",
+    "/db.js",
     "/manifest.webmanifest",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ];
   
 // install
 self.addEventListener("install", function (evt) {
+    // pre cache transaction data
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
-            return cache.addAll(FILES_TO_CACHE);
-        })
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     );
 
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
     self.skipWaiting();
 });
-  
+
+// activate
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
@@ -40,10 +45,9 @@ self.addEventListener("activate", function (evt) {
 
     self.clients.claim();
 });
-  
+
 // fetch
 self.addEventListener("fetch", function (evt) {
-    // cache successful requests to the API
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
@@ -66,11 +70,11 @@ self.addEventListener("fetch", function (evt) {
         return;
     }
 
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
     evt.respondWith(
-        caches.match(evt.request).then(function (response) {
-            return response || fetch(evt.request);
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
         })
     );
 });
