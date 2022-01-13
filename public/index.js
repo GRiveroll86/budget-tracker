19fcec6566e7fdc1f788b425358cb52654d61e83
Greg Riveroll <griveroll86@gmail.com>
2022-01-12 19:34:43 -0500

    prevent default

--- public/index.js
+++ public/index.js
@@ -145,11 +145,9 @@ function sendTransaction(isAdding) {
 }
 
 document.querySelector("#add-btn").onclick = function(event) {
-  event.preventDefault();
   sendTransaction(true);
 };
 
 document.querySelector("#sub-btn").onclick = function(event) {
-  event.preventDefault();
   sendTransaction(false);
 };
